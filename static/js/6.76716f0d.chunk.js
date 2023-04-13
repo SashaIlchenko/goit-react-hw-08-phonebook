c2f1efcade5cfe7222ab689e967f1c35880cea42
SashaIlchenko <109672664+SashaIlchenko@users.noreply.github.com>
2023-04-13 07:18:50 +0000

    Deploying to gh-pages from @ SashaIlchenko/goit-react-hw-08-phonebook@ad8e05d83fefcd67e5ba67d610a8659047a62c95 🚀

---
+++ static/js/6.76716f0d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[6],{6:function(e,t,o){o.r(t);var r=o(329);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Welcome to your phonebook!"})})}}}]);
+//# sourceMappingURL=6.76716f0d.chunk.js.map
